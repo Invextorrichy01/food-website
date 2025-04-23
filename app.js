17ae942d2a02c16aa139f8620de220edddee000f
Clurixtech <akheituamenkellyrichmond@gmail.com>
2025-04-23 12:19:38 +0100

    made some changes

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+require('dotenv').config()
 const express = require('express');
 const expressLayout = require("express-ejs-layouts");
+const connectDB = require("./config/db");
 const app = express();
+
+const PORT = 5000;
+connectDB();
 
 app.use(express.static("./public"))
 app.use(expressLayout)
 app.set('layout', "./layouts/main")
 app.set('view engine', 'ejs')
-
-const PORT = 5000;
 
 app.use("/", require("./routes/main"));
 
